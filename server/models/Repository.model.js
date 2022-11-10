afe2eb28ab96720b1d24bb6a05e306ec94322bee
Intuinum <intuinum@outlook.com>
2022-11-10 15:29:01 -0500

    Added the required option to the schema.

--- server/models/Repository.model.js
+++ server/models/Repository.model.js
@@ -3,6 +3,7 @@ const { Schema, model } = require('mongoose');
 const schema = new Schema({
     name: {
         type: String,
+        required: true,
         unique: true
     },
     documents: [{
